85c9d4ea4ca8a69fb2dafb24be57e6fa00e7eed0
paulpdoa <polopdoandres@gmail.com>
2021-05-11 19:36:46 +0800

    upload to hosting site

--- app/Http/Controllers/PaymentMethodController.php
+++ app/Http/Controllers/PaymentMethodController.php
@@ -8,6 +8,11 @@ use App\Models\PaymentMethod;
 
 class PaymentMethodController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function create(){
         return view('paymentmethods.create');
     }
