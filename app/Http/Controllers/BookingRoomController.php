79ed118058288fc223ef0321f78597e101e61870
paulpdoa <polopdoandres@gmail.com>
2021-05-14 12:04:08 +0800

    yung id paganahin sa add payments

--- app/Http/Controllers/BookingRoomController.php
+++ app/Http/Controllers/BookingRoomController.php
@@ -34,8 +34,8 @@ class BookingRoomController extends Controller
         $records = DB::select(
             "SELECT concat(c.first_name,' ',c.last_name) as customername
             ,concat(g.first_name,' ',g.last_name) as guestname,br.booking_room_id as booking_room_id,
-            rt.room_type,pm.payment_method,p.payment_amount,date_format(b.booked_start_date,'%M %D %Y') as BookedDate,
-            date_format(b.booked_end_date,'%M %D %Y') as EndDate,
+            rt.room_type,pm.payment_method,p.payment_amount,to_char(b.booked_start_date,'Mon-dd-YYYY') as BookedDate,
+            to_char(b.booked_end_date,'Mon-dd-YYYY') as EndDate,
             CASE 
                 WHEN b.booked_start_date < CURDATE() THEN 'Archive'
                 WHEN b.booked_start_date > CURDATE() THEN 'Future'
