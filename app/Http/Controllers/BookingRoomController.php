326d7b3dc6efc64a72377678c052c2e3d48434d4
paulpdoa <polopdoandres@gmail.com>
2021-05-14 12:08:04 +0800

    current date function changed

--- app/Http/Controllers/BookingRoomController.php
+++ app/Http/Controllers/BookingRoomController.php
@@ -37,8 +37,8 @@ class BookingRoomController extends Controller
             rt.room_type,pm.payment_method,p.payment_amount,to_char(b.booked_start_date,'Mon-dd-YYYY') as BookedDate,
             to_char(b.booked_end_date,'Mon-dd-YYYY') as EndDate,
             CASE 
-                WHEN b.booked_start_date < CURDATE() THEN 'Archive'
-                WHEN b.booked_start_date > CURDATE() THEN 'Future'
+                WHEN b.booked_start_date < CURENT_DATE THEN 'Archive'
+                WHEN b.booked_start_date > CURRENT_DATE THEN 'Future'
                 ELSE 'Present'
             END AS History   
             FROM booking_rooms br join bookings b using(booking_id)
